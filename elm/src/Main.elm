f2db00c179e9610a98dd95b6ff1bbf1d786738c1
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2020-02-16 22:04:26 +0100

    try the "buttons" example

---
+++ elm/src/Main.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { counter : Int
+    , clicks : Int
+    }
+
+
+init : Model
+init =
+    { counter = 0, clicks = 0 }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model
+                | counter = model.counter + 1
+                , clicks = model.clicks + 1
+            }
+
+        Decrement ->
+            { model | counter = model.counter - 1, clicks = model.clicks + 1 }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text ("Counter - " ++ String.fromInt model.counter) ]
+        , div [] [ text ("Clicks - " ++ String.fromInt model.clicks) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
