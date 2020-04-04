ede157056d4299220daaa17259bb8dcc436d1aab
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2020-04-04 22:09:50 +0200

    Do some egghed.io exercises

---
+++ egghed-io-using-elm/src/Main.elm
@@ -0,0 +1,15 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import String.Extra exposing (pluralize)
+
+
+items =
+    [ "Green Eggs" ]
+
+
+main =
+    div []
+        [ h1 [] [ text <| pluralize "Item" "Items" (List.length items) ]
+        , text <| Debug.toString <| items
+        ]
